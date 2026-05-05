d330b9fca2601c3a5735eba2094f2cf8bf268ee6
Spectra Source Codes <spectrasourcecode199@gmail.com>
2026-05-04 22:34:12 -0700

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -106,7 +106,7 @@ const Profile = () => {
         animate={{ opacity: 1 }}
         className="pb-20 px-4"
       >
-        <div className="flex justify-between items-center mb-6">
+        <div className="flex justify-between items-center mt-20 mb-6">
           <h1 className="text-2xl font-bold text-primary">My Profile</h1>
           <button
             onClick={isEditing ? handleSave : () => setIsEditing(true)}
